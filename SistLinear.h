d8cba9578d021bd25e8422c609c7575e9619b029
Carmofrasao <anderfrasao52@gmail.com>
2022-03-24 20:51:19 -0300

    ultimate

--- SistLinear.h
+++ SistLinear.h
@@ -3,30 +3,23 @@
 
 #include "utils.h"
 
-/***************************
- ESTRUTURA SISTEMA LINEAR
-****************************/
-/*
-  Método 3: vetor de ponteiros de linhas contíguas
-  http://wiki.inf.ufpr.br/maziero/doku.php?id=prog2:alocacao_dinamica_de_matrizes&s[]=aloca%C3%A7%C3%A3o&s[]=de&s[]=matrizes
-*/
 typedef struct {
   unsigned int num_v;   // numero de variaveis
-  double *M;        // vetor nxn de posições da matriz
-  double **A;       // matriz dos coeficientes do SL (vetor de ponteiros para posições de M)
-  double *b;        // termos independentes do SL
+  double *M;            // vetor nxn de posições da matriz
+  double **A;           // matriz dos coeficientes do SL (vetor de ponteiros para posições de M)
+  double *b;            // termos independentes do SL
 
-  double **L;       // matriz dos multiplicadores (LESS)
-  double **U;       // matriz dos multiplicadores (UPER)
-  double *z;        // vetor z para Fatoração LU
+  double **L;           // matriz dos multiplicadores (LESS)
+  double **U;           // matriz dos multiplicadores (UPER)
+  double *z;            // vetor z para Fatoração LU
 
-  char *eq_aux;        //strings com equacao auxiliar pra montagem da matriz
-  void ***HESSIANA;       //matriz de strings com as expressoes
-  void **GRADIENTE; 
-  double epsilon;   //tolerancia no metodo de gauss seidel
-  int max_iter;     //numero maximo de iterações
-  double *X;        //vetor de resultados finais
-  double tempo;     //tempo de execucao
+  char *eq_aux;         //strings com equacao auxiliar pra montagem da matriz
+  void ***HESSIANA;     //matriz de strings com as expressoes
+  void **GRADIENTE;     //vetor de gradientes
+  double epsilon;       //tolerancia no metodo de gauss seidel
+  int max_iter;         //numero maximo de iterações
+  double *X;            //vetor de resultados finais
+  double tempo;         //tempo de execucao
   double* aprox;        //aproximação inicial
 
 } SistLinear_t;
@@ -38,7 +31,6 @@ void liberaSistLinear(SistLinear_t *SL);
 // devolve um outro Sistema Linear que é uma cópia do SL
 SistLinear_t *dupSL(SistLinear_t *SL);
 
-// ordem de leitura: n A b
 SistLinear_t *lerSistLinear();
 
 #endif
