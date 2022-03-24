249f0855201e7caa348ad4408a6db9ae2b3bf39a
Carmofrasao <anderfrasao52@gmail.com>
2022-03-24 20:54:03 -0300

    ultimate

--- main.c
+++ main.c
@@ -98,6 +98,30 @@ int main (){
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    // cabeçalho
+    printf("Iteração \t| Newton Padrão \t| Newton Modificado \t| Newton Inexato\n");
+
+    // para cada iteração
+    for (...) {
+      printf("%d \t\t| ", i); // imprime iteração
+
+      if (...) {  // se nesta iteração o valor da primeira coluna existe, imprime
+        if (isnan(fx) || isinf(fx))
+          printf("%1.14e\t\t\t| ", fx);
+        else
+          printf("%1.14e\t| ", fx);
+      }
+      else
+        printf("\t\t\t| ");
+
+      // repete para as outras duas colunas...
+    }
+
+    // imprimir os tempos
+    printf("Tempo total \t| %1.14e\t| %1.14e\t| %1.14e\n", TtotalEG, TtotalLU, TtotalGS);
+    printf("Tempo derivadas | %1.14e\t| %1.14e\t| %1.14e\n", TderivadasEG, TderivadasLU, TderivadasGS);
+    printf("Tempo SL \t| %1.14e\t| %1.14e\t| %1.14e\n", TslEG, TslLU, TslGS);
+
     free(X);*/
     ++i;
   }
