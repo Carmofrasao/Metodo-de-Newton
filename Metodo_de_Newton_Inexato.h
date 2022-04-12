059169e2fc8d62118bab9ffd4b9db8c749a6fdae
Pull-eckermann <eric7880@gmail.com>
2022-04-11 23:41:43 -0300

    Inexato alterado de acordo com a orientação do professor

--- Metodo_de_Newton_Inexato.h
+++ Metodo_de_Newton_Inexato.h
@@ -1,10 +1,20 @@
+/*Alunos:
+Anderson Aparecido do Carmo Frasão
+GRR 20204069
+
+Erick Eckermann Cardoso
+GRR20186075
+*/
+
 #ifndef __METODO_DE_NEWTON_INEXATO__
 #define __METODO_DE_NEWTON_INEXATO__
 
 #include "SistLinear.h"
 
+//Função utilizada em Gauss-Seidel que recebe Xi e retorna Xi + 1
 double * calcula_independentes(SistLinear_t *SL, double **m_aux, double *grad, double * res);
 
+// Método Gauss-Seidel para resolução de sistemas lineares
 double * gaussSeidel(SistLinear_t *SL, double **m_aux, double *grad);
 
 //função principal para o metodo newton Inexato
